945cb99298015e072a6fcb6f88b947aa091647b9
Aleksandar Kostadinovski <aleks@acika00mk.local>
2017-07-04 18:04:51 +0200

    service and controller call

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
     /*global
         alert, confirm, console, prompt, angular
     */
-    angular.module('myApp', ['ui.router', 'oc.lazyLoad'])
+    var app = angular.module('myApp', ['ui.router', 'oc.lazyLoad'])
         .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
             $urlRouterProvider.otherwise('/');
             $stateProvider.state('app', {
@@ -25,6 +25,10 @@
                 url: '/user',
                 template: '<usr-comp></usr-comp>',
                 resolve: {
+                    'MyServiceData': function (MyService) {
+// MyServiceData will also be injectable in your controller, if you don't want this you could create a new promise with the $q service
+                        return MyService.promise;
+                    },
                     deps: ['$ocLazyLoad', function ($ocLazyLoad) {
                         return $ocLazyLoad.inject(
                             {
@@ -39,10 +43,40 @@
                );
 
 
+    app.service('MyService', function ($http) {
+        var myData = null, promise = 'data';
+
+        promise = $http.get('https://api.nasa.gov/planetary/apod?api_key=RGsrp9OwHVBgdY08ejeksgfnqxh6tQ7sfigjotdb').then(function (data) {
+            console.log(data);
+            myData = data.data;
+        });
+
+
+        return {
+            promise: promise,
+            setData: function (data) {
+                myData = data;
+            },
+            doStuff: function () {
+                return myData;//.getSomeData();
+            }
+        };
+    });
+
+
+
+    function UserController(MyService) {
+        var self = this;
+
+        console.log('Promise is now resolved: ' + MyService.doStuff());
+        console.log('user');
+
+        this.imageData = MyService.doStuff();
+    }
+
     angular.module('userModule', []).component('usrComp', {
-        template: 'This is the user module <a ui-sref="app">back</a>',
-        controller : function () {
-            console.log('user');
-        }
+        template: '<div>This is the user module <a ui-sref="app">back</a> {{user.imageData}}</div>',
+        controller : UserController,
+        controllerAs: 'user'
     });
 }());
